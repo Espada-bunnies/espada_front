82751d4e31e91df41137044dea1987173182be2f
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2023-04-16 19:39:49 +0300

    feat: add updates

--- src/components/layout/header/header.jsx
+++ src/components/layout/header/header.jsx
@@ -1,37 +1,37 @@
-import React from "react";
-import styles from "./header.module.scss";
-import classNames from "classnames/bind";
+import React from 'react';
+import styles from './header.module.scss';
+import classNames from 'classnames/bind';
 
-import logo from "../../../assets/images/logo.svg";
-import search from "./images/header-search.svg";
+import logo from '../../../assets/images/logo.svg';
+import search from './images/header-search.svg';
 
 const cx = classNames.bind(styles);
 
 const Header = () => {
   return (
-    <header className={cx("header")}>
-      <div className={cx("container", "header__container")}>
+    <header className={cx('header')}>
+      <div className={cx('container', 'header__container')}>
         <a href="/">
           <img src={logo} alt="Espada" loading="eager" />
         </a>
-        <div className={cx("form")}>
-          <form className={cx("search__form")}>
+        <div className={cx('form')}>
+          <form className={cx('search__form')}>
             <input
               type="text"
               placeholder="Поиск"
-              className={cx("search__input")}
+              className={cx('search__input')}
             />
             <img
               src={search}
               loading="eager"
               aria-hidden="true"
               alt=""
-              className={cx("search-image")}
+              className={cx('search-image')}
             />
           </form>
         </div>
-
-        <a href="/" className={cx("link")}>
+        <div></div>
+        <a href="/" className={cx('link')}>
           Войти
         </a>
       </div>
